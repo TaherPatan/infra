5cae812a33fc166341bcc6466a2b51252201df6f
Taher Patanwala <39644647+TaherPatan@users.noreply.github.com>
2025-05-21 15:36:16 +0530

    feat: add description to service_url output

--- modules/k8s/output.tf
+++ modules/k8s/output.tf
@@ -1,3 +1,4 @@
 output "service_url" {
-  value = kubernetes_service.my_app_service.status.0.load_balancer.0.ingress.0.hostname
+  description = "The hostname of the load balancer for the Kubernetes service."
+  value       = kubernetes_service.my_app_service.status.0.load_balancer.0.ingress.0.hostname
 }
