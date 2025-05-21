f0f5a8369bdd5e89363cec2923c6ae01a3893345
Taher Patanwala <39644647+TaherPatan@users.noreply.github.com>
2025-05-21 15:16:15 +0530

    feat: add description to eks_cluster_endpoint variable

--- modules/k8s/variables.tf
+++ modules/k8s/variables.tf
@@ -2,6 +2,9 @@ variable "image_url" {
   description = "The URL of the Docker image to deploy."
   type        = string
 }
-variable "eks_cluster_endpoint" { type = string }
+variable "eks_cluster_endpoint" {
+  description = "The endpoint of the EKS cluster."
+  type        = string
+}
 variable "eks_cluster_ca_cert" { type = string }
 variable "eks_cluster_auth_token" { type = string }
