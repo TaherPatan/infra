030677bdb97aec0ce8d0771de82750c0dee384fd
Taher Patanwala <39644647+TaherPatan@users.noreply.github.com>
2025-05-21 15:19:15 +0530

    feat: add description to eks_cluster_ca_cert variable

--- modules/k8s/variables.tf
+++ modules/k8s/variables.tf
@@ -6,5 +6,8 @@ variable "eks_cluster_endpoint" {
   description = "The endpoint of the EKS cluster."
   type        = string
 }
-variable "eks_cluster_ca_cert" { type = string }
+variable "eks_cluster_ca_cert" {
+  description = "The CA certificate of the EKS cluster."
+  type        = string
+}
 variable "eks_cluster_auth_token" { type = string }
