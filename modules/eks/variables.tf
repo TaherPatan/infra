f37ab6dd2e9a7213b3b443cbf8d807324d67e9d4
Taher Patanwala <39644647+TaherPatan@users.noreply.github.com>
2025-05-21 14:59:07 +0530

    feat: add description to subnet_ids variable

--- modules/eks/variables.tf
+++ modules/eks/variables.tf
@@ -2,4 +2,7 @@ variable "cluster_name" {
   description = "The name of the EKS cluster."
   type        = string
 }
-variable "subnet_ids" { type = list(string) }
+variable "subnet_ids" {
+  description = "A list of subnet IDs to launch the EKS cluster in."
+  type        = list(string)
+}
